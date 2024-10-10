62b496e1077505d5bae35468948a3365772adb09
Artem Dybov <a.dybov@hexway.io>
2024-10-10 21:46:42 +0300

    wef

--- install-master.sh
+++ install-master.sh
@@ -1,2 +1,2 @@
-docker compose run --rm pvb-ovpn-master ovpn_genconfig -u udp://10.5.0.2 -N
+docker compose run --rm pvb-ovpn-master ovpn_genconfig -u udp://127.0.0.1 -N
 docker compose run --rm pvb-ovpn-master ovpn_initpki
