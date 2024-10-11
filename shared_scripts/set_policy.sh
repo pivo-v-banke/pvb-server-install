cb8d5da570c54807489a12dab91e94447eaf1557
XivC <artemd55131@gmail.com>
2024-10-11 20:57:52 +0300

    wtf

---
+++ shared_scripts/set_policy.sh
@@ -0,0 +1,16 @@
+file="../shared/allowed_ips"
+
+# Read the file line by line
+iptables -F
+while IFS= read -r line
+do
+    # Add each IP to the iptables rules
+    iptables -A FORWARD -i tun0 -o eth0 -d "$line" -j ACCEPT
+done < "$file"
+
+# Allow DNS traffic
+iptables -A FORWARD -i tun0 -o eth0 -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -i tun0 -o eth0 -p udp --dport 53 -j ACCEPT
+
+# Reject all other traffic
+iptables -A FORWARD -i tun0 -o eth0 -j REJECT
