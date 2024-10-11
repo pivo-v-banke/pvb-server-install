d7e468ffe9a522edeb5d369c4dd575196c189fc3
XivC <artemd55131@gmail.com>
2024-10-11 21:24:45 +0300

    wtf

--- gen-client-master.sh
+++ gen-client-master.sh
@@ -1,3 +1,3 @@
-docker compose run --rm pvb-ovpn-master easyrsa build-client-full $1 nopass
-docker compose run --rm pvb-ovpn-master ovpn_getclient $1 > clients/$1.ovpn
+docker compose exec pvb-ovpn-master easyrsa build-client-full $1 nopass
+docker compose exec pvb-ovpn-master ovpn_getclient $1 > clients/$1.ovpn
 python3 patch_client.py clients/$1.ovpn
